790b9940bc9f5ed45865fa6aed7c8b4f2f5c670d
Vera Poletaeva <poletaeva.vera.vladimirovna@gmail.com>
2023-10-30 01:03:32 +0300

    fix: change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/rolling-scopes-school/verapoletaeva87-REACT2023Q4',
   plugins: [react()],
 });
