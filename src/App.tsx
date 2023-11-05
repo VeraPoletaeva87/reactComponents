2e0debd09bc2efef531e7057311addcab2c09746
Vera Poletaeva <poletaeva.vera.vladimirovna@gmail.com>
2023-11-05 17:51:45 +0300

    fix: router settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import Detail from './components/detailComponent/detail';
 import Home from './components/homeComponent/home';
 import ErrorPage from './components/errorPage/error';
@@ -8,14 +8,14 @@ import ErrorBoundary from './components/ErrorBoundary';
 function App() {
   return (
     <ErrorBoundary>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />}>
             <Route path="/details/:id" element={<Detail />} />
           </Route>
           <Route path="*" element={<ErrorPage />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </ErrorBoundary>
   );
 }
